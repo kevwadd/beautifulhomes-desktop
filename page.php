bd5be2a2ef0f4ae98ed00266d8e3fc7fc177333e
Kevin Waddell <kevwaddell@mac.com>
2018-05-14 22:22:03 +0100

    Add h1 to content if no post thumbnail

--- page.php
+++ page.php
@@ -13,6 +13,9 @@
 	</div>
 	<?php } ?>
 <div class="container">
+	<?php if (!has_post_thumbnail()) { ?>
+	<h1><?php the_title(); ?></h1>
+	<?php } ?>
 	<?php the_content(); ?>
 </div>
 <?php endwhile; ?>
