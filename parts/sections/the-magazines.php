e32cf72ecaf50092e6e97fbf7e46e476e2695a44
Kevin Waddell <kevwaddell@mac.com>
2018-05-14 22:06:38 +0100

    Fix magazines constant e.g $slide[mag_website_1] to $slide[‘mag_website_1’]

--- parts/sections/the-magazines.php
+++ parts/sections/the-magazines.php
@@ -12,26 +12,26 @@ $last_key = key($comp_slides);
 			<div class="row">
 				<div class="col-4">
 					<?php
-					$img_1 = wp_get_attachment_image_src($slide[mag_logo_1], 'full'); 	
+					$img_1 = wp_get_attachment_image_src($slide['mag_logo_1'], 'full'); 	
 					?>
-					<a href="<?php echo $slide[mag_website_1]; ?>" target="_blank" style="background-image: url(<?php echo $img_1[0]; ?>)" class="block">
-						<span class="sr-only"><?php echo $slide[mag_name_1]; ?></span>	
+					<a href="<?php echo $slide['mag_website_1']; ?>" target="_blank" style="background-image: url(<?php echo $img_1[0]; ?>)" class="block">
+						<span class="sr-only"><?php echo $slide['mag_name_1']; ?></span>	
 					</a>
 				</div>
 				<div class="col-4">
 					<?php
-					$img_2 = wp_get_attachment_image_src($slide[mag_logo_2], 'full'); 	
+					$img_2 = wp_get_attachment_image_src($slide['mag_logo_2'], 'full'); 	
 					?>
-					<a href="<?php echo $slide[mag_website_2]; ?>" target="_blank" style="background-image: url(<?php echo $img_2[0]; ?>)" class="block">
-						<span class="sr-only"><?php echo $slide[mag_name_2]; ?></span>	
+					<a href="<?php echo $slide['mag_website_2']; ?>" target="_blank" style="background-image: url(<?php echo $img_2[0]; ?>)" class="block">
+						<span class="sr-only"><?php echo $slide['mag_name_2]'; ?></span>	
 					</a>
 				</div>
 				<div class="col-4">
 					<?php
-					$img_3 = wp_get_attachment_image_src($slide[mag_logo_3], 'full'); 	
+					$img_3 = wp_get_attachment_image_src($slide['mag_logo_3'], 'full'); 	
 					?>
-					<a href="<?php echo $slide[mag_website_3]; ?>" target="_blank" style="background-image: url(<?php echo $img_3[0]; ?>)" class="block">
-						<span class="sr-only"><?php echo $slide[mag_name_3]; ?></span>	
+					<a href="<?php echo $slide['mag_website_3']; ?>" target="_blank" style="background-image: url(<?php echo $img_3[0]; ?>)" class="block">
+						<span class="sr-only"><?php echo $slide['mag_name_3']; ?></span>	
 					</a>
 				</div>
 			</div>
