33f982f2fdf78bd641a865f2dbf49a956cc02087
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-05-25 15:55:49 +0100

    Working on comments

--- single.php
+++ single.php
@@ -4,4 +4,9 @@
 
 <?php get_template_part( 'parts/blog/post', 'content' ); ?>
 
+<?php if ( comments_open() || get_comments_number() ) { ?>
+<?php comments_template();  ?>		
+<?php } ?>
+
+
 <?php get_footer(); ?>
