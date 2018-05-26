a26d266c79c9b406fe72057c5b5cf5fe42c22d59
Kevin Waddell <kevwaddell@mac.com>
2018-05-26 23:41:26 +0100

    Change slim banners from full width to container size;

--- page-templates/what-we-do-page.php
+++ page-templates/what-we-do-page.php
@@ -10,11 +10,12 @@ Template Name: What We Do Page
 	$feat_img = get_the_post_thumbnail_url($post->ID, 'banner-mb-400-133'); 
 	$feat_img_lg = get_the_post_thumbnail_url($post->ID, 'banner-dt-1500-500');
 	?>
-	<div class="banner-img-slim loading">
-		<div class="container">
-			<h1 class="text-center caps"><span><?php the_title(); ?></span></h1>
+	<div class="banner-img-slim">
+		<div class="container loading">
+			<div class="bg-img" data-src="<?php echo $feat_img_lg; ?>" style="background-image: url(<?php echo $feat_img; ?>)">
+				<h1 class="text-center caps"><span><?php the_title(); ?></span></h1>
+			</div>
 		</div>
-		<div class="bg-img" data-src="<?php echo $feat_img_lg; ?>" style="background-image: url(<?php echo $feat_img; ?>)"></div>
 	</div>
 	<?php } ?>
 <div class="container">
