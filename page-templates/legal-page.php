49817bd149bed81094e1aa2b74bfd378e99cd225
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-06-05 11:58:32 +0100

    Add legal page template

---
+++ page-templates/legal-page.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Legal page template
+*/
+?>
+<?php get_header(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+	<?php if (has_post_thumbnail()) { ?>
+	<?php 
+	$feat_img = get_the_post_thumbnail_url($post->ID, 'banner-mb-400-133'); 
+	$feat_img_lg = get_the_post_thumbnail_url($post->ID, 'banner-dt-1500-500');
+	?>
+	<div class="banner-img-slim">
+		<div class="container loading">
+			<div class="bg-img" data-src="<?php echo $feat_img_lg; ?>" style="background-image: url(<?php echo $feat_img; ?>)"></div>
+		</div>
+	</div>
+	<?php } ?>
+<div class="container">
+	<?php the_content(); ?>
+</div>
+<?php endwhile; ?>
+<?php get_footer(); ?>
