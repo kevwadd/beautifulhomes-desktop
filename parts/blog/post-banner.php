e0cca0f7b118df534534b11168a068bf260e418e
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-06-05 10:59:41 +0100

    Update single post banner

--- parts/blog/post-banner.php
+++ parts/blog/post-banner.php
@@ -3,7 +3,10 @@
 $feat_img = get_the_post_thumbnail_url($post->ID, 'banner-mb-400-133'); 
 $feat_img_lg = get_the_post_thumbnail_url($post->ID, 'banner-dt-1500-500');
 ?>
-<div class="banner-img-slim loading">
-	<div class="bg-img" data-src="<?php echo $feat_img_lg; ?>" style="background-image: url(<?php echo $feat_img; ?>)"></div>
+
+<div class="banner-img-slim">
+	<div class="container loading">
+		<div class="bg-img" data-src="<?php echo $feat_img_lg; ?>" style="background-image: url(<?php echo $feat_img; ?>)"></div>
+	</div>
 </div>
 <?php } ?>
