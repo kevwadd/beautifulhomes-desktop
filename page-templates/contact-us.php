967edc8aedce99598c20efba5c3d0ff074077a2f
Kevin Waddell <kevwaddell@mac.com>
2018-10-08 23:38:10 +0100

    Add proper emails to contact page

--- page-templates/contact-us.php
+++ page-templates/contact-us.php
@@ -9,6 +9,8 @@ Template Name: Contact us
 	<?php 
 	$feat_img = get_the_post_thumbnail_url($post->ID, 'banner-mb-400-133'); 
 	$feat_img_lg = get_the_post_thumbnail_url($post->ID, 'banner-dt-1500-500');
+	$katie_email = get_field('katie_email', 'options');
+	$karen_email = get_field('karen_email', 'options');
 	?>
 	<div class="banner-img-slim">
 		<div class="container loading">
@@ -24,12 +26,12 @@ Template Name: Contact us
 				<div class="col-6 text-center">
 					<h3>For words</h3>
 					<p class="lrg-txt">Karen Wilson</p>
-					<a href="mailto:" class="btn btn-light btn-lg btn-block"><i class="fa fa-envelope pull-left"></i>karen@beautifulhomesnorth.co.uk</a>
+					<a href="mailto:<?php echo $karen_email; ?>" class="btn btn-light btn-lg btn-block"><i class="fa fa-envelope pull-left"></i><?php echo $karen_email; ?></a>
 				</div>
 				<div class="col-6 text-center">
 					<h3>For photography</h3>
 					<p class="lrg-txt">Katie Lee</p>
-					<a href="mailto:" class="btn btn-light btn-lg btn-block"><i class="fa fa-envelope pull-left"></i>katie@beautifulhomesnorth.co.uk</a>
+					<a href="mailto:<?php echo $katie_email; ?>" class="btn btn-light btn-lg btn-block"><i class="fa fa-envelope pull-left"></i><?php echo $katie_email; ?></a>
 				</div>
 			</div>
 		</div>
