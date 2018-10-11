366b832696817049dfa925f552fbe88af0d7b161
Kevin Waddell <kevwaddell@mac.com>
2018-10-12 00:46:40 +0100

    Add tag manager

--- header.php
+++ header.php
@@ -20,6 +20,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PNHPVLZ"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+
 <div id="page" class="site">
 	<a class="sr-only" href="#content">Skip to content</a>
 	
