fc74261b53d49975377082b5e4b327b73bab04b6
Kevin Waddell <kevwaddell@mac.com>
2018-12-27 23:47:02 +0000

    Various style changes to posts.

---
+++ parts/blog/post-sign-up.php
@@ -0,0 +1,6 @@
+<section id="newsletter-form" class="newsletter-sign-up page-section">
+	<h2 class="header-caps text-center"><span>Subscribe</span></h2>
+	<div class="container">
+	<?php echo do_shortcode('[wd_hustle id="post-embed" type="embedded"]'); ?>	
+	</div>
+</section>
--- single.php
+++ single.php
@@ -6,6 +6,8 @@
 
 <?php get_template_part( 'parts/blog/post', 'content' ); ?>
 
+<?php get_template_part( 'parts/blog/post', 'sign-up' ); ?>
+
 <?php get_template_part( 'parts/blog/post', 'related' ); ?>
 
 <?php if ( comments_open() || get_comments_number() ) { ?>
